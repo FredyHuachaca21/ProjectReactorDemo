dc9b62d90f7c7ed946617ab2dbebf617a94305a3
Fredy <fredy.huachaca21@gmail.com>
2021-05-15 12:20:01 -0500

    First Commit

---
+++ src/test/java/com/fhuachaca/app/SpringReactorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhuachaca.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
