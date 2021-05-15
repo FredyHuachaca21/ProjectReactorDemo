c704bd39420875ba9eadca7ca63a28bc0431a523
Fredy <fredy.huachaca21@gmail.com>
2021-05-15 12:31:55 -0500

    Crear Flux

--- src/main/java/com/fhuachaca/app/SpringReactorDemoApplication.java
+++ src/main/java/com/fhuachaca/app/SpringReactorDemoApplication.java
@@ -5,15 +5,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
 public class SpringReactorDemoApplication implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringReactorDemoApplication.class);
+    private static List<String> platos = new ArrayList<>();
 
     public static void main(String[] args) {
+        platos.add("Arroz con pollo");
+        platos.add("Aji de gallina");
+        platos.add("Seco de cordero");
+        platos.add("Lomo saltado");
         SpringApplication.run(SpringReactorDemoApplication.class, args);
     }
 
@@ -22,8 +31,13 @@ public class SpringReactorDemoApplication implements CommandLineRunner {
       monoNumero.subscribe(x -> LOGGER.info("Numero: "+ x));
     }
 
+    public void crearFlux(){
+        Flux<String> fxPlatos = Flux.fromIterable(platos);
+        fxPlatos.subscribe( p -> LOGGER.info(p));
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        crearMono();
+        crearFlux();
     }
 }
